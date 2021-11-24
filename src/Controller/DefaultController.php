04a96c36e6f75dc976b71166bff6919aae884121
Viktor Vinokurov <webkoda@mail.ru>
2021-11-24 14:47:38 +0500

    Adding a product via a controller, connection postgresql

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,25 @@ class DefaultController extends AbstractController
     #[Route('/', name: 'homepage')]
     public function index(): Response
     {
-        return $this->render('main/default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
+        $entityManager = $this->getDoctrine()->getManager();
+        $productList = $entityManager->getRepository(Product::class)->findAll();
+        dd($productList);
+
+        return $this->render('main/default/index.html.twig', []);
+    }
+
+    #[Route('/product-add', name: 'product-add')]
+    public function productAdd(): Response
+    {
+        $product = new Product();
+        $product->setTitle('Product' . rand(1,100));
+        $product->setDescription('description');
+        $product->setPrice(3000);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('homepage');
     }
 }
